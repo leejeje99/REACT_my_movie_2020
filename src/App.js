d3f18b075e20f5b60c3df1f490a27e0dd170f7d7
lee <secret>
2020-09-12 20:21:02 +0900

    #4 CSS&layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-import React from 'react';
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 //중요한 프로토타입
-
+  //펑션예시
 // function Potato() {
 //   return <h1>I like Potato</h1>
 // }
@@ -16,7 +15,7 @@ import PropTypes from "prop-types";
 //   </div>
 //   );
 // }
-
+  //데이터목록
 // const foodILike = [
 //   {
 //     id:1,
@@ -54,12 +53,12 @@ import PropTypes from "prop-types";
 //     rating: 4.5,
 //   }
 // ];
-
+  //렌더링함수 
 // function renderFood(dish){
 //   // console.log(dish) 
 //   return <Food key={dish.id} name={dish.name} picture={dish.image} rating={dish.rating}/>
 // }
-
+  //프로토타입
 // Food.propTypes = {
 //   name: PropTypes.string.isRequired,
 //   picture: PropTypes.string.isRequired,
@@ -67,7 +66,7 @@ import PropTypes from "prop-types";
 
 // }
 
-
+  //메인함수 
 // function App() {
 //   return <div>{foodILike.map(renderFood)}</div>;
 //       /* {foodILike.map(dish => (
@@ -75,41 +74,44 @@ import PropTypes from "prop-types";
 //       ))} */ 
       // }
 
+  //클래스형 메인 
+// class App extends React.Component{
+//   // constructor() {
+//   //   console.log("hello");
+//   // }
+//   state = {
+//     count: 0
+//   };
+//   add = () => { 
+//     console.log("add")
+//     this.setState(current => ({count: current.count + 1}));
+//   };
+//   minus = () => {
+//     console.log("minus")
+//     this.setState(current => ({ count: current.count - 1 }));
+//    };
+//   componentDidMount(){ //1번 
+//     console.log("component rendered")
+//   }
+//   componentDidUpdate(){ //2번 
+//     console.log("I just updated")
+//   }
+//   componentWillUnmount(){ //다른페이지로 갈때 뜸
+//     console.log("goodbye!")
+//   }
+    //렌더링부분 
+//   render(){
+//     console.log("I'm rendering")
+//   return (
+//   <div>
+//     <h1>The Number is : Im a class {this.state.count}</h1>
+//     <button onClick={this.add}>Add</button>
+//     <button onClick={this.minus}>Minus</button>
+//   </div>
+//   )
+//   }
 
-class App extends React.Component{
-  // constructor() {
-  //   console.log("hello");
-  // }
-  state = {
-    count: 0
-  };
-  add = () => { 
-    console.log("add")
-    this.setState(current => ({count: current.count + 1}));
-  };
-  minus = () => {
-    console.log("minus")
-    this.setState(current => ({ count: current.count - 1 }));
-   };
-  componentDidMount(){ //1번 
-    console.log("component rendered")
-  }
-  componentDidUpdate(){ //2번 
-    console.log("I just updated")
-  }
-  componentWillUnmount(){ //다른페이지로 갈때 뜸
-    console.log("goodbye!")
-  }
-  render(){
-    console.log("I'm rendering")
-  return (
-  <div>
-    <h1>The Number is : Im a class {this.state.count}</h1>
-    <button onClick={this.add}>Add</button>
-    <button onClick={this.minus}>Minus</button>
-  </div>
-  )
-  }
+// State 예시
   // state = {
   //   count : 0,
   // };
@@ -128,10 +130,61 @@ class App extends React.Component{
   //     </div>
   //   )
   // }
+// }
+
+
+
+import React from 'react';
+import axios from "axios";
+import Movie from "./Movie";
+import "./App.css"
+
+
+class App extends React.Component {
+  state ={
+    isLoading : true,
+    movies: []
+  };
+  getMovies = async() => {
+    const {
+      data:{
+        data : {movies}
+      }
+    } = await axios.get("https://yts-proxy.now.sh/list_movies.json?sort_by=rating")
+    this.setState({movies, isLoading: false });
+  }
+
+  componentDidMount(){
+    // setTimeout(() => {
+    //   this.setState({isLoading:false})
+    // }, 3000);
+    this.getMovies();
+  }
+  render(){
+    const { isLoading, movies } = this.state;
+    return (
+      <section className="container">
+      {isLoading ? (
+      <div className="loader">
+        <span className="loader__text">Loading...</span>
+      </div>
+      ) : (
+        <div className="movies">
+          {movies.map(movie => (
+            <Movie 
+              key={movie.id} 
+              id={movie.id} 
+              year={movie.year} 
+              title={movie.title} 
+              summary={movie.summary} 
+              poster={movie.medium_cover_image} 
+              genres = {movie.genres}
+            />
+          ))}
+        </div>
+        )}
+      </section>)
+  }
 }
-
-export default App;
-
-
-
  
+export default App;
